93560a3e25868fb33c86a38f8f808f3c4ccabc0c
Szymon Zygmunt <szymonzygmuntzsem@gmail.com>
2025-04-22 19:28:06 +0200

    transition effect fix

--- src/app/components/utils/TransitionLink.js
+++ src/app/components/utils/TransitionLink.js
@@ -1,36 +1,22 @@
 "use client";
-import Link, { LinkProps } from "next/link";
-import React from "react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-interface TransitionLinkProps extends LinkProps {
-  children: React.ReactNode;
-  href: string;
-}
-
-function sleep(ms: number): Promise<void> {
+function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const TransitionLink: React.FC<TransitionLinkProps> = ({
-  children,
-  href,
-  ...props
-}) => {
+export const TransitionLink = ({ children, href, ...props }) => {
   const router = useRouter();
 
-  const handleTransition = async (
-    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ) => {
+  const handleTransition = async (e) => {
     e.preventDefault();
     const body = document.querySelector("body");
 
     body?.classList.add("page-transition");
-
     await sleep(500);
     router.push(href);
     await sleep(500);
-
     body?.classList.remove("page-transition");
   };
 
